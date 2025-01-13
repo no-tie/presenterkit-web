9b4e54c26f8aee35e6af60d404bf1c0f66d8d791
Ali Mammadli <nfsavss@icloud.com>
2025-01-13 10:38:55 +0400

    Update website

--- src/scripts/download.js
+++ src/scripts/download.js
@@ -1,28 +1,15 @@
 import posthog from 'posthog-js'
 
 
-export const initDownload = () => {
+export const initCTA = () => {
     const btnNav = document.getElementById('dwld-btn-nav')
     const btnHero = document.getElementById('dwld-btn-hero')
 
-    const win_url = 'https://github.com/no-tie/presenterkit-app/releases/download/1.0.0/PresenterKit.Setup.1.0.0.exe'
-    const mac_url = 'https://github.com/no-tie/presenterkit-app/releases/download/1.0.0/PresenterKit-1.0.0-arm64.dmg'
-    let dwld_url = ''
-
-    const UA = navigator.userAgent
-
-    if (UA.includes('Win64') || UA.includes('Windows')) { dwld_url = win_url }
-    else if (UA.includes('Mac') || UA.includes('Macintosh')) { dwld_url = mac_url }
-    else {
-        [btnNav, btnHero].map((btn) => {
-            btn.style.backgroundColor = '#7676803E'
-            btn.style.color = '#EBEBF54D'
-            btn.style.cursor = 'default'
-            btn.classList.remove('btn-hover')
-            btn.disabled = true
-        })
+    const goTo = (from) => {
+        posthog.capture('Clicked download', { from })
+        window.open('https://app.presenterkit.app', '_self')
     }
 
-    btnNav.addEventListener('click', () => { posthog.capture('Clicked download', { from: 'Navbar' }); window.open(dwld_url) })
-    btnHero.addEventListener('click', () => { posthog.capture('Clicked download', { from: 'Hero' }); window.open(dwld_url) })
+    btnNav.addEventListener('click', () => goTo('Navbar'))
+    btnHero.addEventListener('click', () => goTo('Hero'))
 }
